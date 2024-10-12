f3705127180ddb3b7207282a4ac699a93eac4c4e
Luiz Verheyen <Luiz.verheyen@student.hogent.be>
2024-10-12 12:53:01 +0200

    end rest intro

---
+++ src/core/logging.ts
@@ -0,0 +1,12 @@
+// src/core/logging.ts
+import winston from 'winston'; 
+
+const rootLogger: winston.Logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+});
+
+export const getLogger = () => {
+  return rootLogger;
+};
